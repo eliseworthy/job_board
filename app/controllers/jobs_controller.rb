c5a2cbac29835503c2f7e5c608cb9b0852866e80
Elise Worthy <elise.worthy@gmail.com>
2015-04-24 15:41:48 -0700

    Adds update action for jobs.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,6 +8,12 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
   def new
     @job = Job.new
   end
